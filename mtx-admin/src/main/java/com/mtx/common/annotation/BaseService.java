fa2d955823123cfcebfb503c910e6e0a2b2346de
yudinggood <1965390348@qq.com>
2019-12-03 16:45:32 +0800

    edit

--- mtx-admin/src/main/java/com/mtx/common/annotation/BaseService.java
+++ mtx-admin/src/main/java/com/mtx/common/annotation/BaseService.java
@@ -5,8 +5,8 @@ import java.lang.annotation.*;
 /**
  * 初始化继承BaseService的service
  */
-@Target({ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
+@Target({ElementType.TYPE})//应用于类、接口（包括注解类型）、枚举
+@Retention(RetentionPolicy.RUNTIME)//注解的生命周期
+@Documented   //使用了@Inherited注解的注解，所标记的类的子类也会拥有这个注解
 public @interface BaseService {
 }
