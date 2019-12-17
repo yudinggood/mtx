7bbe1502320bbcc99939e8c536d9345ab61939ec
yudinggood <1965390348@qq.com>
2019-12-17 20:22:34 +0800

    edit

---
+++ mtx-admin/src/test/java/com/mtx/study/mq/JmsReceiver.java
@@ -0,0 +1,36 @@
+package com.mtx.study.mq;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class JmsReceiver {
+
+    public static void main(String[] args) throws JMSException {
+        // ConnectionFactory ：连接工厂，JMS 用它创建连接
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER,
+                ActiveMQConnection.DEFAULT_PASSWORD, "tcp://127.0.0.1:61616");
+        // JMS 客户端到JMS Provider 的连接
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        // Session： 一个发送或接收消息的线程  false是不带事务
+        Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+        // Destination ：消息的目的地;消息发送给谁.
+        // 获取session注意参数值xingbo.xu-queue是一个服务器的queue，须在在ActiveMq的console配置
+        Destination destination = session.createQueue("my-queue");
+        // 消费者，消息接收者
+        MessageConsumer consumer = session.createConsumer(destination);
+        while (true) {
+            TextMessage message = (TextMessage) consumer.receive();
+            if (null != message) {
+                System.out.println("收到消息：" + message.getText());
+                //session.commit();
+            } else
+                break;
+        }
+        session.close();
+        connection.close();
+    }
+
+}
